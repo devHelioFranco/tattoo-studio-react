7f857eb55e41a1d658b9b3e453d2545beab8713b
Caio <caio-cavalcanti@hotmail.com>
2022-01-06 19:32:32 -0300

    FEAT: criacao pagina tatuadores

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,6 +5,7 @@ import NavBar from "../Nav2"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Home from "../../pages"
 import Agendamento from "../../pages/Agendamento/agendamento"
+import Tatuadores from '../../pages/Tatuadores/Tatuadores'
 import Footer from "../Footer/Footer"
 
 const Layout = () => {
@@ -15,6 +16,7 @@ const Layout = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/agendamento" element={<Agendamento />} />
+          <Route path='/tatuadores' element={ <Tatuadores /> } />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Tatuadores/Tatuadores.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tatuadores = () =>{
+    return(
+        <div>
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default Tatuadores
