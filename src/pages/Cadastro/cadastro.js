7a1af7226335049851c7d0fa46c283948d2eee0a
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2022-01-08 17:16:10 -0300

    route cadastro criada

---
+++ src/pages/Cadastro/cadastro.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+// nome: 'Felipe Santos',
+// 				idade: 18,
+//         endereco: 'Rua pedro ivo 123',
+//         cidade: 'Sao Paulo',
+//         cep: 85780000,
+//         email: 'jose@gmail.com',
+
+const cadastro = () => {
+    return (
+        <h1>Hello world</h1>
+    )
+}
+
+export default cadastro;
