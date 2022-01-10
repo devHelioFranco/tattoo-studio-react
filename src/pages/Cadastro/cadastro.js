ceda87d1fcb26232c87956b5b35a5f2f61f6ef9e
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2022-01-10 17:56:35 -0300

    page de cadastro configurado

--- src/pages/Cadastro/cadastro.js
+++ src/pages/Cadastro/cadastro.js
@@ -1,51 +1,69 @@
-import React from "react";
-import "./cadastro.css"
-import Footer from "../../components/Footer/Footer"
+import React, { useState, } from "react";
+import "./cadastro.css";
+import api from '../../services/api';
 
+function Cadastro() {
+    
+    const [usuario, setUsuario ] = useState({});
 
-const cadastro = () => {
+    function handlechange(e){
+        
+        setUsuario({
+            ...usuario,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(usuario)
+    }
+
     return (
     <div className="container">
-        <form>
+        <form  onSubmit = {handleSubmit}>
+        
         <h2>Cadastro de usuario</h2>
+        
             <div className="row">
                 <span>Nome</span>
-                <input type="text"/>
+                <input type="text" name="Nome" onChange={handlechange}/>
             </div>
+            
             <div className="row">
                 <span>Idade</span>
-                <input type="text"/>
+                <input type="text" name="Idade" onChange={handlechange}/>
             </div>
             <div className="row">
                 <span>Email</span>
-                <input type="text"/>
+                <input type="text"  name="Email" onChange={handlechange}/>
             </div>
             <div className="row">
                 <span>Senha</span>
-                <input type="text"/>
+                <input type="password"  name="Senha" onChange={handlechange}/>
             </div>
             <div className="row">
-                <span>Endereco</span>
-                <input type="text"/>
+                <span>Endereço</span>
+                <input type="text"  name="Endereço" onChange={handlechange}/>
             </div>
             <div className="row">
                 <span>Cidade</span>
-                <input type="text"/>
+                <input type="text" name="Cidade" onChange={handlechange}/>
             </div>
             <div className="row">
                 <span>CEP</span>
-                <input type="text"/>
+                <input type="text"  name="CEP" onChange={handlechange}/>
             </div>
 
             <div className="row">
             <button>CADASTRA-SE </button>
             </div>
         </form>
-        <Footer/>
+        
         </div>
     )
 }
 
 
 
-export default cadastro;
+export default Cadastro;
