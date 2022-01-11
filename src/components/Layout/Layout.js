4970694998b2cb4833a663bc4dae750bb6bc1908
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2022-01-11 02:15:50 -0300

    feat: criacao da pagina contato

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,6 +5,7 @@ import NavBar from "../Nav2"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import Home from "../../pages"
 import Agendamento from "../../pages/Agendamento/agendamento"
+import Contato from '../../pages/Contato/Contato.jsx';
 import Tatuadores from '../../pages/Tatuadores/Tatuadores'
 import Footer from "../Footer/Footer"
 
@@ -15,6 +16,7 @@ const Layout = () => {
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
           <Route path="/agendamento" element={<Agendamento />} />
           <Route path='/tatuadores' element={ <Tatuadores /> } />
         </Routes>
---
+++ src/pages/Contato/Contato.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Caio from '../../assets/caio.jpeg';
+import Evelyn from '../../assets/evelyn.jpg';
+import Helio from '../../assets/helio.jpg';
+import Hyan from '../../assets/hyan.jpg';
+import Mariana from '../../assets/mariana.jpg';
+import style from './style.css';
+
+
+const Contato = () =>{
+    return(
+        <section className="grid">
+            <div>
+                <h1 className='tituloGrid'>Caio</h1>
+                <img
+                    src={Caio}
+                    className='cardImagem'
+                    />
+                <h3><a href='#'>Linkdin</a></h3>
+                <h3><a href='#'>Github</a></h3>   
+            </div>
+            <div className='card'>
+                <h1 className='tituloGrid'>Evelyn</h1>
+                <img
+                    src={Evelyn}
+                    className='cardImagem'
+                    />
+                <h3><a href='#'>Linkdin</a></h3>
+                <h3><a href='#'>Github</a></h3>        
+            </div>
+            <div className='card'>
+                <h1 className='tituloGrid'>Helio</h1>
+                <img
+                    src={Helio}
+                    className='cardImagem'
+                    />
+                <h3><a href='#'>Linkdin</a></h3>
+                <h3><a href='#'>Github</a></h3>    
+            </div>
+            <div className='card'>        
+                <h1 className='tituloGrid'>Hyan</h1>
+                <img
+                    src={Hyan}
+                    className='cardImagem'
+                    />
+                <h3><a href='#'>Linkdin</a></h3>
+                <h3><a href='#'>Github</a></h3>    
+            </div>
+                <div className='card'>
+                <h1 className='tituloGrid'>Mariana</h1>
+                <img
+                    src={Mariana}
+                    className='cardImagem'
+                    />
+                <h3><a href='#'>Linkdin</a></h3>
+                <h3><a href='#'>Github</a></h3>   
+            </div>
+        </section>
+    )
+}
+
+export default Contato
