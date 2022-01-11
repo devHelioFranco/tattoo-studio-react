1a776ead571c9497b32c7e775a8e7c124a9927a3
Hyan Araujo <88167146+B34tdb@users.noreply.github.com>
2022-01-11 16:16:40 -0300

    Cep

--- src/pages/Cadastro/cadastro.js
+++ src/pages/Cadastro/cadastro.js
@@ -1,5 +1,6 @@
 import React, { useState, } from "react";
 import "./cadastro.css";
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import api from "../../api/api"
 
 function Cadastro() {
@@ -21,11 +22,40 @@ function Cadastro() {
         });
     }
 
-    return (
-    <div className="container">
-        <form  onSubmit = {handleSubmit}>
+    function onBlurCep(ev, setFieldValue){
         
-        <h2>Cadastro de usuario</h2>
+        const {value} = ev.target;
+
+        const cep = value?.replace(/[^0-9]/g, '');
+
+        if (cep?.length !== 8 ){
+            return;
+        }
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then((res) => res.json())
+                .then((data) => {
+                     setFieldValue("localidade", data.cidade);
+                    setFieldValue("logradouro", data.endereco);
+                });
+    }
+    
+    return (
+        <>
+        <Formik
+        onSubmit={onSubmit}
+        validateOnMount
+        initialValues={{
+          cep: '',
+          logradouro: '',
+          cidade: '',
+        }}
+        
+            {/* <div className="container"> */}
+                       
+            render={({ isValid, setFieldValue }) => (
+          
+            <form  onSubmit = {handleSubmit}>
+            <h2>Cadastro de usuario</h2>
         
             <div className="row">
                 <span>Nome</span>
@@ -45,6 +75,11 @@ function Cadastro() {
                 <input type="password"  name="senha" onChange={handlechange}/>
             </div>
             <div className="row">
+                <span>CEP</span>
+                <input type="text"  name="cep" onChange={handlechange} onBlur={(ev)=> onBlurCep(ev,setFieldValue)}/>
+            </div>
+
+            <div className="row">
                 <span>Endereço</span>
                 <input type="text"  name="endereco" onChange={handlechange}/>
             </div>
@@ -52,18 +87,17 @@ function Cadastro() {
                 <span>Cidade</span>
                 <input type="text" name="cidade" onChange={handlechange}/>
             </div>
+            
             <div className="row">
-                <span>CEP</span>
-                <input type="text"  name="cep" onChange={handlechange}/>
+            <button disabled={!isValid}>CADASTRA-SE </button>
             </div>
-
-            <div className="row">
-            <button>CADASTRA-SE </button>
-            </div>
-        </form>
-        
-        </div>
+            </form>
+            )}
+            
+            // </div>
+          </>
     )
+    
 }
 
 
