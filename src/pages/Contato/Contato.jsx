8a071e8b5d5706b470a2d77ff215251a787dd57f
Caio Cavalcanti <caio-cavalcanti@hotmail.com>
2022-01-12 17:33:40 -0300

    fix:correcoes imagem e links da pagina contato

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Caio from '../../assets/caio.jpeg';
-import Evelyn from '../../assets/evelyn.jpg';
-import Helio from '../../assets/helio.jpg';
-import Hyan from '../../assets/hyan.jpg';
-import Mariana from '../../assets/mariana.jpg';
+import Evelyn from '../../assets/evelyn.jpeg';
+import Helio from '../../assets/helio.jpeg';
+import Hyan from '../../assets/hyan.jpeg';
+import Mariana from '../../assets/mariana.jpeg';
 import style from './style.css';
 
 
@@ -16,8 +16,8 @@ const Contato = () =>{
                     src={Caio}
                     className='cardImagem'
                     />
-                <h3><a href='#'>Linkdin</a></h3>
-                <h3><a href='#'>Github</a></h3>   
+                <h3><a href='https://www.linkedin.com/in/caio-cavalcanti-17b50b13a/'>Linkdin</a></h3>
+                <h3><a href='https://github.com/CaioSix'>Github</a></h3>   
             </div>
             <div className='card'>
                 <h1 className='tituloGrid'>Evelyn</h1>
@@ -25,8 +25,8 @@ const Contato = () =>{
                     src={Evelyn}
                     className='cardImagem'
                     />
-                <h3><a href='#'>Linkdin</a></h3>
-                <h3><a href='#'>Github</a></h3>        
+                <h3><a href='https://www.linkedin.com/in/evelynbalves/'>Linkdin</a></h3>
+                <h3><a href='https://github.com/evelynsba'>Github</a></h3>        
             </div>
             <div className='card'>
                 <h1 className='tituloGrid'>Helio</h1>
@@ -34,8 +34,8 @@ const Contato = () =>{
                     src={Helio}
                     className='cardImagem'
                     />
-                <h3><a href='#'>Linkdin</a></h3>
-                <h3><a href='#'>Github</a></h3>    
+                <h3><a href='https://www.linkedin.com/in/dev-heliofranco/'>Linkdin</a></h3>
+                <h3><a href='https://github.com/devHelioFranco'>Github</a></h3>    
             </div>
             <div className='card'>        
                 <h1 className='tituloGrid'>Hyan</h1>
@@ -43,8 +43,8 @@ const Contato = () =>{
                     src={Hyan}
                     className='cardImagem'
                     />
-                <h3><a href='#'>Linkdin</a></h3>
-                <h3><a href='#'>Github</a></h3>    
+                <h3><a href='https://www.linkedin.com/in/hyan-araujo-55a9891b2/'>Linkdin</a></h3>
+                <h3><a href='https://github.com/B34tdb'>Github</a></h3>    
             </div>
                 <div className='card'>
                 <h1 className='tituloGrid'>Mariana</h1>
@@ -52,8 +52,8 @@ const Contato = () =>{
                     src={Mariana}
                     className='cardImagem'
                     />
-                <h3><a href='#'>Linkdin</a></h3>
-                <h3><a href='#'>Github</a></h3>   
+                <h3><a href='https://www.linkedin.com/in/mariana-cristina-santos/'>Linkdin</a></h3>
+                <h3><a href='https://github.com/mariana-cristina-santos'>Github</a></h3>   
             </div>
         </section>
     )
